44f0c1dbf701af03a2a2f5a678dbd7ce13a78931
sean15535 <sean15535@gmail.com>
2024-03-18 21:33:15 +0100

    fixed the guess limit error code

--- functions.py
+++ functions.py
@@ -1,18 +1,18 @@
 import random 
 
 def play():
-  guess_limit = 3 
-  guess = 0
-  while guess > guess_limit :
-     user= str(input("Enter choice 'h' for head while 't' for tail: ")).lower()
-     output = random.choice(['h', 't'])
-     computer = output
-     if user == computer:
-        print(f"The coin fliped on{computer}You Won !")
-     if user != computer:
-        print(f"The coin fliped on{computer} You Lost !")
-     guess +=1
-  return "Guess Limit Exceeded , Try again"
+    guess_limit = 3 
+    while guess_limit > 0 :
+        user = str(input("Enter choice 'h' for head while 't' for tail: ")).lower()
+        output = random.choice(['h', 't'])
+        computer = output
+        if user == computer:
+            print(f"The coin flipped on {computer}. You Won!")
+            return
+        else:
+            guess_limit -= 1
+            print(f"The coin flipped on {computer}. You Lost! You have {guess_limit} guesses left.")
+    print("Chances exceeded. Game Over")
 
 def about():
     print("This is a simple coin flip game. You can guess whether the coin will land on heads or tails.")
